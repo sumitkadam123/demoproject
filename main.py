f2f8bdda70e647b12abda47e864bc98f01dddbee
sumitkadam123 <sumitkadam5801@gmail.com>
2026-04-22 12:05:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+##write a python function which take user input as a number and
+## will print the table of that number.
+def generate_table(number):
+    for i in range (1.11):
+        print(f*(number) * (i) - (number * i))
+
+        generate table (10)
